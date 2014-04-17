9c4255368826c6fd25e80b09132bf07176e07b97
showmen15 <showmen15@poczta.onet.pl>
2014-04-17 11:45:15 +0200

    tt

--- Lokalizacja/Lokalizacja.cpp
+++ Lokalizacja/Lokalizacja.cpp
@@ -1,10 +1,11 @@
 // Lokalizacja.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 
+#include <stdio.h>
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
 	return 0;
 	
